e392c0b0db6eb869a4b685c2ef446ba74b848e85
Matheus Ferreira <matheus.galhardo98@gmail.com>
2026-03-05 17:54:53 +0000

    File changes

--- src/pages/MyCRM.jsx
+++ src/pages/MyCRM.jsx
@@ -4,9 +4,7 @@ import { useCorporateAccess } from "@/components/hooks/useCorporateAccess";
 import { PIPELINE_STAGES, STAGE_COLORS, CRM_TYPES } from "@/components/ui/DesignTokens";
 import { StageBadge } from "@/components/shared/StatusBadge";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Loader2, X, ExternalLink, Trash2, Lightbulb, EyeOff } from "lucide-react";
-import TaskDrawer from "@/components/crm/TaskDrawer";
 import TaskDrawer from "@/components/crm/TaskDrawer";
 import { createPageUrl } from "@/utils";
 
